ea69fe33d8fd9cf29a0f2525d58e9d9a961aefe5
khizarmd <mohommadkhizar2@gmail.com>
2023-11-26 13:34:08 +0530

    add tic tac toe game code

---
+++ index.js
@@ -0,0 +1,121 @@
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
+
+let currentPlayer;
+let gameGrid;
+
+const winningPositions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+// lets create a function to initialize the game
+
+function initGame(){
+    currentPlayer = "X";
+    gameGrid = ["","","","","","","","",""];
+    boxes.forEach((box,index) => {
+       box.innerText ="";
+       boxes[index].style.pointerEvents = "all"
+    //    ui se green color remove krna h game over hone ke bad(initialize css property again)
+        box.classList = `box box${index+1}`        
+       
+    })
+    
+    newGameBtn.classList.remove("active");
+    gameInfo.innerHTML = `Current Player - ${currentPlayer}`
+}
+
+initGame();
+
+function swapTurn(){
+    if(currentPlayer === "X"){
+        currentPlayer = "O";
+    }
+    else{
+        currentPlayer="X"
+    }
+    // UI update
+    gameInfo.innerHTML = `Current Player - ${currentPlayer}`
+}
+
+
+
+function checkGameOver(){
+   let answer = "";
+   
+   winningPositions.forEach((position)=>{
+    // all 3 boxes should be non empty and exactly same
+     if((gameGrid[position[0]] !=="" || gameGrid[position[1]] !== "" || gameGrid[position[2]] !== "")
+     && (gameGrid[position[0]] === gameGrid[position[1]] ) && (gameGrid[position[1]] === gameGrid[position[2]]))
+            {
+                // check if winner is X
+                if(gameGrid[position[0]] === "X")
+                     answer="X";
+
+                else
+                    answer = "O";
+                
+                    // disable pointer event
+                    boxes.forEach((box)=>{
+                        box.style.pointerEvents = "none"
+                    })
+                
+                // now we know who is winner
+                boxes[position[0]].classList.add("win");
+                boxes[position[1]].classList.add("win");
+                boxes[position[2]].classList.add("win");
+            }
+    })
+    // if we have winner
+    if(answer!==""){
+        gameInfo.innerText = `Winner Player - ${answer}`
+        newGameBtn.classList.add("active");
+        return
+    }
+    // when there is no winner or tie
+
+    let fillCount = 0;
+    gameGrid.forEach((box)=>{
+        if(box!==""){
+            fillCount++;
+        }
+    })
+
+    // agr sare box no empty h tb fillcount = 9
+    
+    if(fillCount===9){
+        gameInfo.innerText ="Game Tied"
+        newGameBtn.classList.add("active")
+    }
+}
+
+function handleClick(index){
+     if(gameGrid[index] === ""){
+        boxes[index].innerText = currentPlayer;
+        gameGrid[index] = currentPlayer;
+        boxes[index].style.pointerEvents = "none"
+        // swap kro turn ko
+        swapTurn();
+        // check koi jeet to nhi gya
+        checkGameOver();
+        
+     }
+}
+
+boxes.forEach((box, index)=>{
+    box.addEventListener("click",()=>{
+        handleClick(index);
+    })
+})
+
+newGameBtn.addEventListener("click",initGame);
+
+
